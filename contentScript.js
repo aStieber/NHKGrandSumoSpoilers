eb08c85328b6fb599dea46d009dc6b2c2a1e3e56
astieber <astieber@taittowers.com>
2019-05-17 17:57:55 -0600

    initial commit

---
+++ contentScript.js
@@ -0,0 +1,6 @@
+"use strict";
+
+window.addEventListener('DOMContentLoaded', function (){
+	var spoilerDiv = document.getElementById("p-tournament__vs");
+	spoilerDiv.classList.add("spoilerBlur");
+}, false);
