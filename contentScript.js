3d05a36d67d0ed33550ed89e9207197648877d12
astieber <astieber@taittowers.com>
2019-05-17 19:17:39 -0600

    added click to toggle, removed red on winner when blurred.

--- contentScript.js
+++ contentScript.js
@@ -3,4 +3,8 @@
 window.addEventListener('DOMContentLoaded', function (){
 	var spoilerDiv = document.getElementById("p-tournament__vs");
 	spoilerDiv.classList.add("spoilerBlur");
+
+	spoilerDiv.onclick = function () {
+		spoilerDiv.classList.toggle("spoilerBlur");
+	};
 }, false);
